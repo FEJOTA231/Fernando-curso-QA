93ca977a35bdd570b09d89b9ed3ce2630fc50bcf
Fernando Francisco <Fejota2005@gmail.com>
2025-10-30 19:18:30 -0300

    "Adicionando arquivos"

---
+++ Evitando_Problemas/consultacep.py
@@ -0,0 +1,17 @@
+import requests
+
+integrantes = {
+    "Dione": "11633422",
+    "Pacheco": "11633010"
+}
+
+for nome, cep in integrantes.items():
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+    resposta = requests.get(url)
+
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        cidade = dados.get("localidade", "Cidade não encontrada")
+        print(f"{nome} mora em {cidade}.")
+    else: 
+        print(f"Não foi possível localizar os dados de {nome}.")

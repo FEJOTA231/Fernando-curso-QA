8854e5aa27a5eb4ef14930b3972c0b2d56a7bb7b
Fernando Francisco <Fejota2005@gmail.com>
2025-10-30 19:38:37 -0300

    "Novas atividades"

---
+++ Variávéis_e_Funções/desafio_caique2.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+altura = input("Insira sua altura em metros: ")
+idade = input("Insira sua idade: ")
+
+print(f"Olá {nome}, sua altura é {altura} metros, e sua idade é {idade}.")
